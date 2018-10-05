8ad2fba63d84ab87e0b0052af5b93a93b07f28cb
pvacey <pvacey@gmail.com>
2018-10-05 14:48:15 -0500

    inital improvements for scripted deployment

--- create-resources.sh
+++ create-resources.sh
@@ -1,8 +1,37 @@
 #!/usr/bin/env bash
+
+# replace placeholder values in the code
 echo "enter password for RCON:"
 read -s NEW_PASSWORD
 sed -i "s/PLACEHOLDER_PASSWORD/${NEW_PASSWORD}/g" instance.yml
+sed -i "s/PLACEHOLDER_PASSWORD/${NEW_PASSWORD}/g" cloud-function/main.py
 sed -i "s/PROJECT_ID/${GOOGLE_CLOUD_PROJECT}/g" instance.yml
+sed -i "s/PLACEHOLDER_PROJECT_ID/${GOOGLE_CLOUD_PROJECT}/g" cloud-function/main.py
+
+# create the minecraft instance and open firewall rules
 gcloud deployment-manager deployments create mc --config instance.yml
+
+# retrieve the server IP
 IP=$(gcloud compute instances describe minecraft-vm --zone us-central1-f | grep natIP | cut -d\: -f2)
-echo "Server IP: $IP"
+echo
+echo "Minecraft Server IP: $IP"
+echo
+
+# create a pubsub topic for the interesting minecraft logs
+gcloud beta pubsub topics create minecraft-logs
+
+# create filters to send events to the pubsub topic
+gcloud beta logging sinks create minecraft-chat-logs pubsub.googleapis.com/projects/minecraft-212921/topics/minecraft-logs --log-filter='jsonPayload.instance.name="minecraft-vm" "]: <"'
+gcloud beta logging sinks create minecraft-disconnect-logs pubsub.googleapis.com/projects/minecraft-212921/topics/minecraft-logs --log-filter='jsonPayload.instance.name="minecraft-vm" "left the game"'
+
+# grant sink identities access to pubsub
+TMP1=$(gcloud beta logging sinks describe minecraft-chat-logs | grep writerIdentity | cut -d" " -f2)
+TMP2=$(gcloud beta logging sinks describe minecraft-disconnect-logs | grep writerIdentity | cut -d" " -f2)
+gcloud beta pubsub topics add-iam-policy-binding minecraft-logs --member $TMP1 --role roles/pubsub.publisher
+gcloud beta pubsub topics add-iam-policy-binding minecraft-logs --member $TMP2 --role roles/pubsub.publisher
+exit
+
+# deploy cloud function
+cd cloud-function
+gcloud beta functions deploy minecraft-bot --entry-point log_handler --runtime python37 --trigger-topic minecraft-logs
+cd ..
